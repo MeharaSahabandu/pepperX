00f0f43f66c97b6419ae95b1c559d50e81107052
Mehara Sahabandu <sahabandumehara@gmail.com>
2023-10-16 11:53:58 +0530

    Manager home created

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import ManagerHome from "./components/ManagerHome";
+import SupervisorHome from "./components/SupervisorHome";
 
 export default function App() {
-  return <ManagerHome />;
+  return <SupervisorHome />;
 }
