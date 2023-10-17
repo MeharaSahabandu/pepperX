7a9e91b9305209cc3331bf121c1b62ee61fe51a1
IT21016684 <110347124+KanishkaGJ@users.noreply.github.com>
2023-10-18 01:47:17 +0530

    modal in income component

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -9,6 +9,8 @@ import PlantationDetails from "../components/PlantationDetails";
 import PlantationDataList from "../components/PlantationDataList";
 import MaintenanceDetails from "../components/MaintenanceDetails";
 import LandOwner from "../components/LandOwner";
+import LandOwnerIncome from "../components/LandOwnerIncome";
+import LandOwnerHI from "../components/LandOwnerHI";
 import AllZones from "../components/AllZones";
 import AllExpenditure from "../components/AllExpenditure";
 
@@ -17,11 +19,15 @@ const Stack = createNativeStackNavigator();
 function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="LandOwner">
         <Stack.Screen name="All Income" component={AllIncome} />
         <Stack.Screen name="All Expenditure" component={AllExpenditure} />
         <Stack.Screen name="All Zones" component={AllZones} />
+
         <Stack.Screen name="LandOwner" component={LandOwner} />
+        <Stack.Screen name="LandOwnerIncome" component={LandOwnerIncome} />
+        <Stack.Screen name="LandOwnerHI" component={LandOwnerHI} />
+
         <Stack.Screen name="ManagerHome" component={ManagerHome} />
 
         <Stack.Screen name="SupervisorHome" component={SupervisorHome} />
