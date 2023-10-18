5870926ffc0701d36c6e6ed8d0009c04dc136788
IT21016684 <110347124+KanishkaGJ@users.noreply.github.com>
2023-10-19 00:16:49 +0530

    picker addition

--- components/PlantationDetails.js
+++ components/PlantationDetails.js
@@ -6,11 +6,11 @@ import {
   TouchableOpacity,
   Modal,
   TextInput,
-  Picker,
   Platform,
   StyleSheet,
   KeyboardAvoidingView,
 } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import DatePicker from "react-native-modern-datepicker";
 import { getFormatedDate } from "react-native-modern-datepicker";
 import { collection, query, getDocs, addDoc } from "firebase/firestore";
