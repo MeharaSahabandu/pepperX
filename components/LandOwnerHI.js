e465de87ba259b20e48736e1f4d2b84c8f67c34c
IT21016684 <110347124+KanishkaGJ@users.noreply.github.com>
2023-10-19 00:34:46 +0530

    alot of chanages

--- components/LandOwnerHI.js
+++ components/LandOwnerHI.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { 
-    View, 
-    Text, 
-    ScrollView, 
-    TouchableOpacity, 
-    Modal, 
-    TextInput, 
-    Picker, 
-    Platform, 
-    StyleSheet,
-    KeyboardAvoidingView, 
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Modal,
+  TextInput,
+  Picker,
+  Platform,
+  StyleSheet,
+  KeyboardAvoidingView,
 } from "react-native";
 // import DatePicker from 'react-native-date-picker';
 // import ModalDateTime from 'react-native-modal-datetime-picker';
@@ -21,39 +21,34 @@ import { db } from "./config";
 
 export default function LandOwnerHI() {
   const [incomeData, setIncomeData] = useState([]);
-
   const [isAddPopupVisible, setIsAddPopupVisible] = useState(false);
   const [newIncome, setNewIncome] = useState("");
   const [newQty, setNewQty] = useState("");
-  const [selectedZone, setSelectedZone] = useState('A'); // Default value
-//   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedZone, setSelectedZone] = useState("A"); // Default value
 
-const [openStartDatePicker, setOpenStartDatePicker] = useState(false);
-const today = new Date();
-const startDate = getFormatedDate(
-      today.setDate(today.getDate() + 1),
-      "YYYY/MM/DD"
-    );
-const [selectedStartDate, setSelectedStartDate] = useState("");
-const [startedDate, setStartedDate] = useState("12/12/2023");
+  const [openStartDatePicker, setOpenStartDatePicker] = useState(false);
+  const today = new Date();
+  const startDate = getFormatedDate(
+    today.setDate(today.getDate() + 1),
+    "YYYY/MM/DD"
+  );
+  const [selectedStartDate, setSelectedStartDate] = useState("");
+  const [startedDate, setStartedDate] = useState("12/12/2023");
 
-
-function handleChangeStartDate(propDate) {
+  function handleChangeStartDate(propDate) {
     setStartedDate(propDate);
   }
 
-const handleOnPressStartDate = () => {
+  const handleOnPressStartDate = () => {
     setOpenStartDatePicker(!openStartDatePicker);
-};
+  };
 
-  const zones = ['A', 'B', 'C', 'D'];
-
-  
+  const zones = ["A", "B", "C", "D"];
 
   const showAddPopup = () => {
     setIsAddPopupVisible(true);
   };
-  
+
   const hideAddPopup = () => {
     setIsAddPopupVisible(false);
   };
@@ -105,13 +100,13 @@ const handleOnPressStartDate = () => {
   const getZoneBackgroundColor = (zone) => {
     switch (zone) {
       case "A":
-        return styles.zoneRecA; // Define the style for Zone A background color
+        return styles.zoneRecA;
       case "B":
-        return styles.zoneRecB; // Define the style for Zone B background color
+        return styles.zoneRecB;
       case "C":
-        return styles.zoneRecC; // Define the style for Zone C background color
+        return styles.zoneRecC;
       case "D":
-        return styles.zoneRecD; // Define the style for Zone D background color
+        return styles.zoneRecD;
       default:
         return {};
     }
@@ -144,17 +139,19 @@ const handleOnPressStartDate = () => {
 
   return (
     <>
-    <KeyboardAvoidingView
-          behavior={Platform.OS == "ios" ? "padding" : ""}
-          style={{
-            width: "100%",
-            height: "100%",
-            backgroundColor: "#fff",
-          }}
-        >
-    <TouchableOpacity style={styles.addButton} onPress={showAddPopup}>
-        <Text style={styles.addButtonText}>+</Text>
-    </TouchableOpacity>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : ""}
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#fff",
+        }}
+      >
+        <TouchableOpacity style={styles.addButton} onPress={showAddPopup}>
+          <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+
+        
 
     <Modal
     animationType="slide"
@@ -195,84 +192,91 @@ const handleOnPressStartDate = () => {
                 >
                   <Text placeholder="Select Date">{selectedStartDate}</Text>
                 </TouchableOpacity>
-        </View>
-        <TouchableOpacity style={styles.ModaladdButton} onPress={handleAddIncome}>
-            <Text style={{ color: 'white' }}>Add</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.ModalcancelButton} onPress={hideAddPopup}>
-            <Text style={{ color: 'white' }}>Cancel</Text>
-        </TouchableOpacity>
-        </View>
-    </View>
-    </Modal>
-
-    {/* Create modal for date picker */}
-
-    <Modal
-              animationType="slide"
-              transparent={true}
-              visible={openStartDatePicker}
-            >
-              <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                  <DatePicker
-                    mode="calendar"
-                    minimumDate={startDate}
-                    selected={startedDate}
-                    onDateChanged={handleChangeStartDate}
-                    onSelectedChange={(date) => setSelectedStartDate(date)}
-                    options={{
-                      backgroundColor: "#F0FFFF",
-                      textHeaderColor: "#05AF6D",
-                      textDefaultColor: "#05AF6D",
-                      selectedTextColor: "#FFF",
-                      mainColor: "#05AF6D",
-                      textSecondaryColor: "#FFFFFF",
-                      borderColor: "rgba(122, 146, 165, 0.1)",
-                    }}
-                  />
-  
-                  <TouchableOpacity onPress={handleOnPressStartDate}>
-                    <Text style={{ color: "#05AF6D" }}>Done</Text>
-                  </TouchableOpacity>
-                </View>
               </View>
-    </Modal>
+              <TouchableOpacity
+                style={styles.ModaladdButton}
+                onPress={handleAddIncome}
+              >
+                <Text style={{ color: "white" }}>Add</Text>
+              </TouchableOpacity>
 
-    {/* End of modal for date picker */}
-
-    <ScrollView>
-      {incomeData.map((item, index) => (
-        <View key={index} style={styles.rectangle}>
-          <Text style={styles.date}>
-            <Text style={{ fontSize: 25 }}>12</Text>
-            <br />
-            June
-          </Text>
-          <View style={styles.separator} />
-          <Text style={styles.text}>
-            <Text style={{ color: "#888888", fontSize: 14 }}>
-              Income Generated
-            </Text>
-            <br />
-            <Text style={{ color: "black", fontSize: 24 }}>
-              LKR, {item.income}
-            </Text>
-          </Text>
-          <View style={styles.columnContainer}>
-            <View style={[styles.zoneRec, getZoneBackgroundColor(item.zone)]}>
-              <Text style={styles.zoneRecText}>{`Zone ${item.zone}`}</Text>
-            </View>
-            <View style={styles.qtyRec}>
-              <Text style={styles.qtyRecText}>{`QTY ${item.qty}`}</Text>
+              <TouchableOpacity
+                style={styles.ModalcancelButton}
+                onPress={hideAddPopup}
+              >
+                <Text style={{ color: "white" }}>Cancel</Text>
+              </TouchableOpacity>
             </View>
           </View>
-        </View>
-      ))}
-    </ScrollView>
-    </KeyboardAvoidingView>
+        </Modal>
+
+        {/* Create modal for date picker */}
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={openStartDatePicker}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <DatePicker
+                mode="calendar"
+                minimumDate={startDate}
+                selected={startedDate}
+                onDateChanged={handleChangeStartDate}
+                onSelectedChange={(date) => setSelectedStartDate(date)}
+                options={{
+                  backgroundColor: "#F0FFFF",
+                  textHeaderColor: "#05AF6D",
+                  textDefaultColor: "#05AF6D",
+                  selectedTextColor: "#FFF",
+                  mainColor: "#05AF6D",
+                  textSecondaryColor: "#FFFFFF",
+                  borderColor: "rgba(122, 146, 165, 0.1)",
+                }}
+              />
+
+              <TouchableOpacity onPress={handleOnPressStartDate}>
+                <Text style={{ color: "#05AF6D" }}>Done</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+        {/* End of modal for date picker */}
+
+        <ScrollView>
+          {incomeData.map((item, index) => (
+            <View key={index} style={styles.rectangle}>
+              <Text style={styles.date}>
+                <Text style={{ fontSize: 25 }}>12</Text>
+                <br />
+                June
+              </Text>
+              <View style={styles.separator} />
+              <Text style={styles.text}>
+                <Text style={{ color: "#888888", fontSize: 14 }}>
+                  Income Generated
+                </Text>
+                <br />
+                <Text style={{ color: "black", fontSize: 24 }}>
+                  LKR, {item.income}
+                </Text>
+              </Text>
+              <View style={styles.columnContainer}>
+                <View
+                  style={[styles.zoneRec, getZoneBackgroundColor(item.zone)]}
+                >
+                  <Text style={styles.zoneRecText}>{`Zone ${item.zone}`}</Text>
+                </View>
+                <View style={styles.qtyRec}>
+                  <Text style={styles.qtyRecText}>{`QTY ${item.qty}`}</Text>
+                </View>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </KeyboardAvoidingView>
     </>
   );
 }
-
--- components/MaintenanceDataList.js
+++ components/MaintenanceDataList.js
@@ -1,31 +1,76 @@
-import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import { collection, query, getDocs } from "firebase/firestore";
 import { styles } from "../css/PlantationDetailsCSS";
+import { db } from "./config";
 
 export default function MaintenanceDataList() {
+  const [maintenanceData, setMaintenanceData] = useState([]);
+
+  const getZoneBackgroundColor = (zone) => {
+    switch (zone) {
+      case "A":
+        return styles.zoneRecA;
+      case "B":
+        return styles.zoneRecB;
+      case "C":
+        return styles.zoneRecC;
+      case "D":
+        return styles.zoneRecD;
+      default:
+        return {};
+    }
+  };
+
+  useEffect(() => {
+    const fetchMaintenanceData = async () => {
+      try {
+        const q = query(collection(db, "maintainEx"));
+        const querySnapshot = await getDocs(q);
+
+        const data = [];
+        querySnapshot.forEach((doc) => {
+          data.push(doc.data());
+        });
+
+        setMaintenanceData(data);
+      } catch (error) {
+        console.error("Error fetching maintenance data: ", error);
+      }
+    };
+
+    fetchMaintenanceData();
+  }, []);
+
   return (
     <View>
-      <View style={styles.rectangle}>
-        <Text style={styles.date}>
-          <Text style={{ fontSize: 25 }}>12</Text>
-          <br />
-          June
-        </Text>
-        <View style={styles.separator}></View>
-        <Text style={styles.text}>
-          <Text style={{ color: "#888888", fontSize: 12 }}>Watering</Text>{" "}
-          <br />
-          LKR 55,000
-          <br />
-          <br />
-          <Text style={{ color: "#888888", fontSize: 12 }}>Trimming</Text>
-          <br />
-          LKR 20,000
-        </Text>
-        <View style={styles.zoneLabel}>
-          <Text style={styles.zoneLabelTextA}> Zone A </Text>
+      {maintenanceData.map((item, index) => (
+        <View key={index} style={styles.rectangle}>
+          <Text style={styles.date}>
+            <Text style={{ fontSize: 25 }}>12</Text>
+            {"\n"}
+            June
+          </Text>
+          <View style={styles.separator} />
+          <Text style={styles.text}>
+            <Text style={{ color: "#888888", fontSize: 12 }}>Wages Paid</Text>
+            <br />
+            {`LKR ${item.wages}`}
+            <br />
+            <br />
+            <Text style={{ color: "#888888", fontSize: 12 }}>
+              Other Expenditure
+            </Text>
+            <br />
+            {`LKR ${item.other}`}
+          </Text>
+          <View style={styles.columnContainer}>
+            <View style={[styles.zoneRec, getZoneBackgroundColor(item.zone)]}>
+              <Text style={styles.zoneRecText}>Zone {item.zone}</Text>
+            </View>
+          </View>
         </View>
-      </View>
+      ))}
     </View>
   );
 }
