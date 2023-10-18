ffa68d3b8ac8d70e5b26026d4ed791d5749eff62
Kulanaka Bandara <mkulanakabandara@gmail.com>
2023-10-18 21:45:42 +0530

    commiting the updated machine operations

--- components/MachineOperations.js
+++ components/MachineOperations.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "./Header";
+import { View, TouchableOpacity, Text, Modal } from 'react-native';
 
 const MachineOperations = () => {
+  const [open, setOpen] = useState(false);
+
+  function handleOnPress () {
+    setOpen(!open);
+  }
+
   return (
-    <Header />
+    <>
+      <Header />
+      <View>
+        <TouchableOpacity onPress={handleOnPress}>
+          <Text>
+            Open
+          </Text>
+        </TouchableOpacity>
+        <Modal
+        animationType='slide'
+        transparent={true}
+        visible={open}
+        >
+
+        </Modal>
+      </View>
+    </>
   )
 }
 
